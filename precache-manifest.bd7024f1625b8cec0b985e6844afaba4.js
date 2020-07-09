8fd3c78f4dac15544fdb6e9bdfc9dc03c6aea43f
anael <anael.mashinsky@gmail.com>
2020-07-09 16:14:24 +0300

    Updates

---
+++ precache-manifest.bd7024f1625b8cec0b985e6844afaba4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46ffcd26d85ba8d7d580bce622e35552",
+    "url": "/tic-tac-toe-React/index.html"
+  },
+  {
+    "revision": "88f9d607619fab1c710c",
+    "url": "/tic-tac-toe-React/static/css/main.46b64de4.chunk.css"
+  },
+  {
+    "revision": "4e5d039475d49936bdbf",
+    "url": "/tic-tac-toe-React/static/js/2.52e14435.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tic-tac-toe-React/static/js/2.52e14435.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f9d607619fab1c710c",
+    "url": "/tic-tac-toe-React/static/js/main.9797b7b9.chunk.js"
+  },
+  {
+    "revision": "803e293051fbcd454810",
+    "url": "/tic-tac-toe-React/static/js/runtime-main.8b0d0ef2.js"
+  },
+  {
+    "revision": "321be728147a4b5d26d86ff1bbca894b",
+    "url": "/tic-tac-toe-React/static/media/1.321be728.png"
+  },
+  {
+    "revision": "b20696dbacbe1f3b513f5cec4d228cd9",
+    "url": "/tic-tac-toe-React/static/media/2.b20696db.png"
+  },
+  {
+    "revision": "ebfaeacb429f8672fd8d3cb3f314431f",
+    "url": "/tic-tac-toe-React/static/media/3.ebfaeacb.png"
+  },
+  {
+    "revision": "5fbc9cefa051c90f50dc2f81cfde0260",
+    "url": "/tic-tac-toe-React/static/media/4.5fbc9cef.png"
+  },
+  {
+    "revision": "fc86478d2f1a28df8af3e76625d4d85d",
+    "url": "/tic-tac-toe-React/static/media/5.fc86478d.png"
+  },
+  {
+    "revision": "8d39babb4d7bda6999cf45a4a8acaf06",
+    "url": "/tic-tac-toe-React/static/media/6.8d39babb.png"
+  },
+  {
+    "revision": "af300c212b65835660dd8330eedb8228",
+    "url": "/tic-tac-toe-React/static/media/background.af300c21.jpg"
+  },
+  {
+    "revision": "9e6085965b946e1df28be9625e8eae68",
+    "url": "/tic-tac-toe-React/static/media/logo.9e608596.png"
+  }
+]);
